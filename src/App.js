0262360d1397445f63b6932eb5bdf44f957bc878
Kk492000 <kunalkamthe200@gmail.com>
2024-06-08 07:57:32 +0530

    used Class components instead of functional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Suspense } from "react";
+import React, { Component, Suspense } from "react";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
 
 const UserList = React.lazy(() => import("./components/ui/UserList"));
 
-function App() {
-  return (
-    <div>
-      <Suspense
-        fallback={
-          <div>
-            <Loader />
-          </div>
-        }
-      >
-        <UserList />
-      </Suspense>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Suspense
+          fallback={
+            <div>
+              <Loader />
+            </div>
+          }
+        >
+          <UserList />
+        </Suspense>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ui/UserList.js
+++ src/components/ui/UserList.js
@@ -1,49 +1,69 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import Loader from "../Loader/Loader";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, withStyles } from "@material-ui/core";
 import User from "./User";
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { fetchUsers } from "../../store/userSlice";
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   userGridContainer: {
     width: "100%",
     margin: "0 auto",
   },
-}));
+});
 
-const UserList = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const users = useSelector((state) => state.users.users);
-  const status = useSelector((state) => state.users.status);
-  const error = useSelector((state) => state.users.error);
-
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchUsers());
+class UserList extends Component {
+  componentDidMount() {
+    if (this.props.status === "idle") {
+      this.props.fetchUsers();
     }
-  }, [status, dispatch]);
-
-  if (status === "loading") {
-    return <Loader />;
-  }
-  if (status === "failed") {
-    let errorMsg;
-    if (error) {
-      errorMsg = error;
-    }
-    return errorMsg;
   }
 
-  return (
-    <Grid container spacing={3} className={classes.userGridContainer}>
-      {users.map((user) => {
-        const avatarUrl = `https://api.dicebear.com/8.x/avataaars/svg?seed=${user.username}`;
-        return <User key={user.id} user={user} diceBearUrl={avatarUrl} />;
-      })}
-    </Grid>
-  );
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.status !== this.props.status &&
+      this.props.status === "idle"
+    ) {
+      this.props.fetchUsers();
+    }
+  }
+
+  render() {
+    const { classes, users, status, error } = this.props;
+
+    if (status === "loading") {
+      return <Loader />;
+    }
+    if (status === "failed") {
+      let errorMsg;
+      if (error) {
+        errorMsg = error;
+      }
+      return errorMsg;
+    }
+
+    return (
+      <Grid container spacing={3} className={classes.userGridContainer}>
+        {users.map((user) => {
+          const avatarUrl = `https://api.dicebear.com/8.x/avataaars/svg?seed=${user.username}`;
+          return <User key={user.id} user={user} diceBearUrl={avatarUrl} />;
+        })}
+      </Grid>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  users: state.users.users,
+  status: state.users.status,
+  error: state.users.error,
+});
+
+const mapDispatchToProps = {
+  fetchUsers,
 };
 
-export default UserList;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(UserList));
